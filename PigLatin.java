3c4b7460091d38d49d984deeb5d0c0c809ed460a
despiegelman <mtvesuvius67@gmail.com>
2024-09-25 23:22:53 -0400

    Update PigLatin.java

--- PigLatin.java
+++ PigLatin.java
@@ -11,10 +11,15 @@ public class PigLatin
       	Scanner input = new Scanner(System.in);
 		System.out.print("Enter a string: ");
 		String str = input.nextLine();
-        System.out.print(FigureOutWord.isVowel(str));	 	
-		String piggie = FigureOutWord.convertWord(str);
-		System.out.println();
-		System.out.println("Converted word: " + piggie);
+		input.close();
+		if (str.indexOf(" ") == -1){
+			String piggie = FigureOutWord.convertWord(str);
+			System.out.println("Converted word: " + piggie);
+		}
+		else{
+			String piggie = FigureOutWord.convertSentence(str);
+			System.out.println("Converted sentence: " + piggie);
+		}
 	}
 }
 
@@ -26,7 +31,7 @@ class FigureOutWord
 	 * Postcondition: returns a word converted into Pig Latin according to the appropriate rule.
  	 * 
 	 */
-    public static boolean isVowel(String s){
+	public static boolean startsVowel(String s){
         s = s.toUpperCase();
         if (s.substring(0, 1).equals("A") || s.substring(0, 1).equals("E") || s.substring(0, 1).equals("I") || s.substring(0, 1).equals("O") || s.substring(0, 1).equals("U")){
             return true;
@@ -35,9 +40,63 @@ class FigureOutWord
             return false;
         }
     }
+	public static boolean startsConsonant(String s){
+        s = s.toUpperCase();
+        if (s.substring(0, 1).equals("A") || s.substring(0, 1).equals("E") || s.substring(0, 1).equals("I") || s.substring(0, 1).equals("O") || s.substring(0, 1).equals("U")){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    public static boolean isVowel(String s){
+        s = s.toUpperCase();
+        if (s.equals("A") || s.equals("E") || s.equals("I") || s.equals("O") || s.equals("U")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+	public static boolean startsTwoConsonants(String s){
+		s.toUpperCase();
+		if (FigureOutWord.startsConsonant(s)){
+			if (FigureOutWord.isVowel(s.substring(1,2))){
+				return false;
+			}
+			else{
+				return true;
+			}
+		}
+		else{
+			return false;
+		}
+	}
 	public static String convertWord(String s)
 	{
+		if (FigureOutWord.startsConsonant(s) && FigureOutWord.startsTwoConsonants(s)){
+			return s.substring(2) + s.substring(0,2) + "ay";
+		}
+		else if (FigureOutWord.startsConsonant(s)){
+			return s.substring(1) + s.substring(0,1) + "ay";
+		}
+		else if (FigureOutWord.startsVowel(s)){
+			return s + "way";
+		}
      		 return "";      // This statement is provided to allow initial compiling.
+	}  
+	public static String convertSentence(String s)
+	{
+		int num = 0;
+		int num1 = s.indexOf(" ");
+		String endingStr = "";
+		while (num1 > -1){
+			endingStr += FigureOutWord.convertWord(s.substring(num, num1)) + " ";
+			num = num1+1;
+			num1 = s.indexOf(" ", num);
+		}
+		endingStr += FigureOutWord.convertWord(s.substring(num, s.length()-1));
+		return (endingStr);
 	}  
    
 }
